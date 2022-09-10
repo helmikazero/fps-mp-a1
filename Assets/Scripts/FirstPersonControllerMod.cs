862706afc03ab32988a6f7f5192035066bc14da0
Helmika Mahendra Priyanto <helmika.zero@gmail.com>
2022-09-10 20:38:19 +0700

    Update FirstPersonControllerMod.cs

--- Assets/Scripts/FirstPersonControllerMod.cs
+++ Assets/Scripts/FirstPersonControllerMod.cs
@@ -55,7 +55,7 @@ namespace StarterAssets
 		private float _cinemachineTargetPitch;
 
 		// player
-		[SerializeField]private float _speed;
+		private float _speed;
 		private float _rotationVelocity;
 		private float _verticalVelocity;
 		private float _terminalVelocity = 53.0f;
@@ -159,9 +159,8 @@ namespace StarterAssets
 			// a simplistic acceleration and deceleration designed to be easy to remove, replace, or iterate upon
 
 			// note: Vector2's == operator uses approximation so is not floating point error prone, and is cheaper than magnitude
-
 			// if there is no input, set the target speed to 0
-			/*if (_input.move == Vector2.zero) targetSpeed = 0.0f;*/
+			if (_input.move == Vector2.zero) targetSpeed = 0.0f;
 
 			// a reference to the players current horizontal velocity
 			float currentHorizontalSpeed = new Vector3(_controller.velocity.x, 0.0f, _controller.velocity.z).magnitude;
@@ -170,7 +169,7 @@ namespace StarterAssets
 			float inputMagnitude = _input.analogMovement ? _input.move.magnitude : 1f;
 
 			// accelerate or decelerate to target speed
-			/*if (currentHorizontalSpeed < targetSpeed - speedOffset || currentHorizontalSpeed > targetSpeed + speedOffset)
+			if (currentHorizontalSpeed < targetSpeed - speedOffset || currentHorizontalSpeed > targetSpeed + speedOffset)
 			{
 				// creates curved result rather than a linear one giving a more organic speed change
 				// note T in Lerp is clamped, so we don't need to clamp our speed
@@ -182,13 +181,7 @@ namespace StarterAssets
 			else
 			{
 				_speed = targetSpeed;
-			}*/
-
-			_speed = Mathf.Lerp(currentHorizontalSpeed, targetSpeed * inputMagnitude, Time.deltaTime * SpeedChangeRate);
-
-			// round speed to 3 decimal places
-			_speed = Mathf.Round(_speed * 1000f) / 1000f;
-
+			}
 
 
 			// normalise input direction
